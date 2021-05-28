5641caccf7f47c255ce8c262d2778e9b086b4fdf
Kumaran Baskaran <kumar.physics@gmail.com>
2021-05-28 11:26:43 -0400

    Initial commit

---
+++ bayesian-validation/bayesian-validation.py
@@ -0,0 +1,13 @@
+from mmcif.io.PdbxReader import PdbxReader
+import pynmrstar
+import sys
+
+
+class BayesianValidation(object):
+    def __init__(self):
+        pass
+
+    def read_cif(self,cif_file):
+        pass
+    def distance_matrix(self,coordinates):
+        pass
